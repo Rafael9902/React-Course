6fe2c508c38137b90d613822606cdd8d4f71d857
Rafael <rafaelortiz99@outlook.com>
2022-04-23 16:52:47 -0500

    Gif app

---
+++ 02-gif-expert-app/src/components/gifExpertApp/GiftExperApp.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import { AddCategory } from '../addCategory/AddCategory';
+import { GifsGrid } from '../gifsGrid/GifsGrid';
+
+export const GiftExperApp = () => {
+  const [categories, setCategories] = useState(['One Punch']);
+
+  return (
+    <div>
+        <h2>GiftExperApp</h2>
+        <AddCategory setCategories={ setCategories } />
+        <hr />
+        <ol>
+          {categories.map(category => <GifsGrid key={category} category={category}/>)}
+        </ol>
+        <GifsGrid category={categories}/>
+        {/* <ol>
+          { categories.map((category, i) => <li key={category}>{category}</li>) }
+        </ol> */}
+    </div>
+  )
+}
---
+++ 02-gif-expert-app/src/components/gifsGrid/GifsGrid.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const GifsGrid = ({ category }) => {
+    //const getGifs =  
+    return (
+        <h3>
+            {category}
+        </h3>
+    )
+}
