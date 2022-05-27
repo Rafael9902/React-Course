f1276ccb5d07f4fec5a65a252cda025c635950e7
Rafael <rafaelortiz99@outlook.com>
2022-05-27 11:17:11 -0500

    form

--- heroes-app/src/components/hero/HeroScreen.jsx
+++ heroes-app/src/components/hero/HeroScreen.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useParams, Navigate } from "react-router-dom";
 import { getHeroesById } from "../../selectors/getHeroesById";
 
 export const HeroScreen = () => {
   const { id } = useParams();
-  const hero = getHeroesById(id);
+  const hero = useMemo(() => getHeroesById(id), [id]);
+
+
 
   if(!hero) {
     return <Navigate to='/' />;
--- heroes-app/src/components/hero/HeroesList.jsx
+++ heroes-app/src/components/hero/HeroesList.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { getHeroesByPublisher } from "../../selectors/getHeroesByPublisher";
 import { HeroCard } from "./HeroCard";
 
 export const HeroesList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
+
   const validPublishers = ['DC Comics', 'Marvel Comics'];
 
   if(!validPublishers.includes(publisher)) {
