e121cdbd57b76267befb78196e05936134e26784
Rafael Ortiz <rafaelortiz99@outlook.com>
2022-06-14 10:52:55 -0500

    calendar

--- calendar/calendar-app/src/components/calendar/pages/CalendarPage.tsx
+++ calendar/calendar-app/src/components/calendar/pages/CalendarPage.tsx
@@ -1,9 +1,42 @@
-import { Navbar } from "../../shared/navbar/Navbar"
+import { Navbar } from "../../shared/navbar/Navbar";
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { addHours } from "date-fns";
+import { localizer, getMessages} from "../../../helpers";
 
 export const CalendarPage = () => {
+  
+  const events = [{
+    title: 'birthday',
+    notes: 'buy dessert',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+      _id: '123',
+      name: 'rafael'
+    }
+  }]
+
+  const eventStyleGetter = ( event: any, start: any, end: any, isSelected: any ) => {
+    
+  }
+
+  
   return (
     <>
       <Navbar />
+
+      <Calendar
+      culture='es'
+      localizer={localizer}
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+      messages={ getMessages() }
+      eventPropGetter={ eventStyleGetter }
+    />
     </>
   )
 }
