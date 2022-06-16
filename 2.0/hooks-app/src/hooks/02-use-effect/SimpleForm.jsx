bbc2dabeefa28449740f63ac5998283eebdd1736
Rafael Ortiz <rafaelortiz99@outlook.com>
2022-06-16 15:00:28 -0500

    useeffect

--- 2.0/hooks-app/src/hooks/02-use-effect/SimpleForm.jsx
+++ 2.0/hooks-app/src/hooks/02-use-effect/SimpleForm.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { Message } from "./Message";
 
 export const SimpleForm = () => {
     const [formState, setFormState] = useState({
@@ -21,14 +22,14 @@ export const SimpleForm = () => {
     //eso no es eficiente, son los efectos secundarios
     //se coloca como segundo parametro un arreglo vacio, si queremos que se ejecute solo al inicio del ciclo de vida del componente(mouunt)
 
-    useEffect(() => console.log('use'), [formState])
-
   return (
     <>
         <h1>Simple Form</h1>
 
         <input type='text' placeholder='name' name='username' value={username} onChange={ onInputChange }/>
         <input type='email' placeholder='email' name='email' value={email} onChange={ onInputChange }/>
+
+        { username === 'rafael' && <Message /> }
     </>
   )
 }
